cf586cf8f8037f671a10587e451ff82186958840
Stratos Staboulis <stratos.staboulis@gmail.com>
2025-12-30 15:39:37 +0200

    Initial commit

---
+++ bjj-open-mats/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-blue-600 p-6">
+        BJJ Open Mats
+      </h1>
+    </div>
+  );
+}
+
+export default App;
